da698df3be2419bf809a7a2d93bd01b9eb80ce06
ansonfoong <afoong@albany.edu>
2019-07-24 01:38:25 -0400

    Check to make sure user exists by e-mail or username.

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    res.sendStatus(200);
+router.post('/', async (req, res) => {
+    // Validate all of the body data.
+    // const errors = validationResult(req);
+    // console.log(errors);
+    let user = await User.findOne({ $or: [ { username: req.body.username }, { email: req.body.email }]});
+    if(user) {
+        res.sendStatus(409);
+        console.log("User already exists.");
+        console.log(user);
+    }
+    else {
+        let user = await (new User(req.body)).save(); // Need to hash user's passwords, then we're good.
+        console.log("User Saved.");
+        console.log(user);
+    }
+
 });
 
 module.exports = router;
